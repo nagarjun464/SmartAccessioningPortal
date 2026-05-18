aa1be2abe9934ab6b4ec06dcca21992c1f3756ca
Nagarjun-Test <smartnag341@gmail.com>
2026-05-18 03:03:40 -0500

    add tube photo domain entity

---
+++ SmartAccessioningPortal.Domain/Entities/TubePhoto.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartAccessioningPortal.Domain.Entities;
+
+public class TubePhoto
+{
+    public int TubePhotoId { get; set; }
+    public int CaseId { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+    public DateTime CapturedAt { get; set; } = DateTime.UtcNow;
+}
