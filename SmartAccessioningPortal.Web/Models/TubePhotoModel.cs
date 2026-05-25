4fc99bc269cf5fdec66cd288f6896bd3fab3485e
Nagarjun-Test <smartnag341@gmail.com>
2026-05-24 20:58:44 -0500

    add tube photo model for case details and load tube photos in case details page and load tube photos in case details page and added tube photo preview modal in case details

---
+++ SmartAccessioningPortal.Web/Models/TubePhotoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartAccessioningPortal.Web.Models;
+
+public class TubePhotoModel
+{
+    public int TubePhotoId { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public DateTime CapturedAt { get; set; }
+}
